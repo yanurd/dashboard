6e42ed4efab650512ec5f1d15c5c935fa40f2304
Yancarlo Urdaneta <yancarlou@gmail.com>
2021-08-21 00:31:18 -0400

    Display card js file

---
+++ assets/js/displayCards.js
@@ -0,0 +1,93 @@
+var cards = [
+  {
+    icon: "",
+    class: "facebook",
+    user: "nathanf",
+    followers: 1987,
+    change: 12,
+    changeClass: "up",
+  },
+  {
+    icon: "",
+    class: "twitter",
+    user: "nathanf",
+    followers: 1044,
+    change: 99,
+    changeClass: "up",
+  },
+  {
+    icon: "",
+    class: "instagram",
+    user: "realnathanf",
+    followers: "11k",
+    change: 1099,
+    changeClass: "up",
+  },
+  {
+    icon: "",
+    class: "youtube",
+    user: "Nathan F.",
+    followers: 8239,
+    change: 144,
+    changeClass: "down",
+  },
+];
+
+var overview = [
+  {
+    icon: "facebook",
+    title: "Page Views",
+    content: 87,
+    change: 3,
+    changeClass: "up",
+  },
+  {
+    icon: "facebook",
+    title: "Likes",
+    content: 52,
+    change: 2,
+    changeClass: "down",
+  },
+  {
+    icon: "instagram",
+    title: "Likes",
+    content: 5462,
+    change: 2257,
+    changeClass: "up",
+  },
+  {
+    icon: "instagram",
+    title: "Profile Views",
+    content: "52k",
+    change: 1375,
+    changeClass: "up",
+  },
+  {
+    icon: "twitter",
+    title: "Retweets",
+    content: 117,
+    change: 303,
+    changeClass: "up",
+  },
+  {
+    icon: "twitter",
+    title: "Likes",
+    content: 507,
+    change: 553,
+    changeClass: "up",
+  },
+  {
+    icon: "youtube",
+    title: "Likes",
+    content: 107,
+    change: 19,
+    changeClass: "down",
+  },
+  {
+    icon: "youtube",
+    title: "Total Views",
+    content: 1407,
+    change: 12,
+    changeClass: "down",
+  },
+];
