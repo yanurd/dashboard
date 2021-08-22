80c7a8a9edafc9efdc0f1e8f6174402ce418c908
Yancarlo Urdaneta <yancarlou@gmail.com>
2021-08-21 22:09:30 -0400

    Layout done

--- assets/js/displayCards.js
+++ assets/js/displayCards.js
@@ -1,124 +1,172 @@
-
 const bigCardsOverview = () => {
   var cards = [
     {
-      icon: "",
+      icon: "assets/img/icon-facebook.svg",
+      altIcon: "Facebook Logo",
       class: "facebook",
-      user: "nathanf",
+      user: "@nathanf",
       followers: 1987,
+      typeFollowers: 'followers',
       change: 12,
+      changeIcon: "assets/img/icon-up.svg",
       changeClass: "up",
     },
     {
-      icon: "",
+      icon: "assets/img/icon-twitter.svg",
+      altIcon: "Twitter Logo",
       class: "twitter",
-      user: "nathanf",
+      user: "@nathanf",
       followers: 1044,
+      typeFollowers: 'followers',
       change: 99,
+      changeIcon: "assets/img/icon-up.svg",
       changeClass: "up",
     },
     {
-      icon: "",
+      icon: "assets/img/icon-instagram.svg",
+      altIcon: "Instagram Logo",
       class: "instagram",
-      user: "realnathanf",
+      user: "@realnathanf",
       followers: "11k",
+      typeFollowers: 'followers',
       change: 1099,
+      changeIcon: "assets/img/icon-up.svg",
       changeClass: "up",
     },
     {
-      icon: "",
+      icon: "assets/img/icon-youtube.svg",
+      altIcon: "Youtube Logo",
       class: "youtube",
       user: "Nathan F.",
       followers: 8239,
+      typeFollowers: 'subscribers',
       change: 144,
+      changeIcon: "assets/img/icon-down.svg",
       changeClass: "down",
     },
   ];
   var bigCards = document.getElementById("big-cards");
-  cards.forEach(card => {
+  cards.forEach((card) => {
     bigCards.innerHTML += `
-    <div class="${card.class}">
-      <p>${card.user}</p>
-      <p>${card.followers}</p>
-      <p class="${card.changeClass}">${card.change}</p>
+    <div class="big-overview dark-bg ${card.class}">
+      <p>
+        <img 
+          src="${card.icon}"
+          alt="${card.altIcon}"
+          height="25px"
+        />
+        ${card.user}
+      </p>
+      <div>
+        <p>${card.followers}</p>
+        <p>${card.typeFollowers}</p>
+      </div>
+      <p class="${card.changeClass}">
+        <img src="${card.changeIcon}" alt="${card.changeClass}" />
+        ${card.change}
+      </p>
     </div>
-    `
-  })
-}
+    `;
+  });
+};
 
 const smallCardsOverview = () => {
   var overview = [
     {
-      icon: "facebook",
+      icon: "assets/img/icon-facebook.svg",
+      altIcon: "Facebook Logo",
       title: "Page Views",
       content: 87,
       change: 3,
+      changeIcon: "assets/img/icon-up.svg",
       changeClass: "up",
     },
     {
-      icon: "facebook",
+      icon: "assets/img/icon-facebook.svg",
+      altIcon: "Facebook Logo",
       title: "Likes",
       content: 52,
       change: 2,
+      changeIcon: "assets/img/icon-down.svg",
       changeClass: "down",
     },
     {
-      icon: "instagram",
+      icon: "assets/img/icon-instagram.svg",
+      altIcon: "Instagram Logo",
       title: "Likes",
       content: 5462,
       change: 2257,
+      changeIcon: "assets/img/icon-up.svg",
       changeClass: "up",
     },
     {
-      icon: "instagram",
+      icon: "assets/img/icon-instagram.svg",
+      altIcon: "Instagram Logo",
       title: "Profile Views",
       content: "52k",
       change: 1375,
+      changeIcon: "assets/img/icon-up.svg",
       changeClass: "up",
     },
     {
-      icon: "twitter",
+      icon: "assets/img/icon-twitter.svg",
+      altIcon: "Twitter Logo",
       title: "Retweets",
       content: 117,
       change: 303,
+      changeIcon: "assets/img/icon-up.svg",
       changeClass: "up",
     },
     {
-      icon: "twitter",
+      icon: "assets/img/icon-twitter.svg",
+      altIcon: "Twitter Logo",
       title: "Likes",
       content: 507,
       change: 553,
+      changeIcon: "assets/img/icon-up.svg",
       changeClass: "up",
     },
     {
-      icon: "youtube",
+      icon: "assets/img/icon-youtube.svg",
+      altIcon: "Youtube Logo",
       title: "Likes",
       content: 107,
       change: 19,
+      changeIcon: "assets/img/icon-down.svg",
       changeClass: "down",
     },
     {
-      icon: "youtube",
+      icon: "assets/img/icon-youtube.svg",
+      altIcon: "Youtube Logo",
       title: "Total Views",
       content: 1407,
       change: 12,
+      changeIcon: "assets/img/icon-down.svg",
       changeClass: "down",
     },
   ];
   var smallCards = document.getElementById("small-cards");
-  overview.forEach( card => {
+  overview.forEach((card) => {
     smallCards.innerHTML += `
-      <div>
-        <p>${card.title}</p>
-        <p>${card.content}</p>
-        <p>${card.icon}</p>
-        <p>${card.change}</p>
-      </div>`
-  })
-}
-
+      <div class="overview-card dark-bg">
+        <p class="card-subtitle desaturated">${card.title}</p>
+        <img 
+          src="${card.icon}" 
+          alt="${card.altIcon}" 
+          height="25px"
+        />
+        <p class="overview-card_text">${card.content}</p>
+        <span class="${card.changeClass}">
+          <img 
+          src="${card.changeIcon}" 
+          alt="${card.changeClass}"/>
+          ${card.change}%
+        </span>
+      </div>`;
+  });
+};
 
 const displayCards = () => {
-  bigCardsOverview()
-  smallCardsOverview()
-}
+  bigCardsOverview();
+  smallCardsOverview();
+};
